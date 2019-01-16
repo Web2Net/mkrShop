478c3fa36fb7a3eb67dca0932fcd696653c42e80
Yurk_in_home <vitalyyou@gmail.com>
2019-01-16 09:03:18 +0200

    Разделил индекс-логи на месяцы

--- index.php
+++ index.php
@@ -1,35 +1,22 @@
 <?php
-
-//phpinfo();
-
 ini_set('display_errors', 'On'); // сообщения с ошибками будут показываться
 error_reporting(E_ALL); // E_ALL - отображаем ВСЕ ошибки
 //$value = $var[$key]; // пример ошибки
 //ini_set('display_errors', 'Off'); // теперь сообщений НЕ будет
-/*
-session_start();
-unset($_SESSION); 
-session_destroy();
-//header('Location: index.php')
-
-$MySQL_log=getenv("DOCUMENT_ROOT")."/magic/MySQL.log";
-//unlink($filename);
-$filewrite=fopen($MySQL_log,"w+");
-$paper='';
-fwrite($filewrite,$paper);
-fclose($filewrite);
-*/
 
 include_once ("kernel/cfg.php");		
 include_once ("site/logic.php");	
 
-$file = getenv("DOCUMENT_ROOT") . "/magic/index.log";
+$period = date("Y_m");
+
+$index_log = getenv("DOCUMENT_ROOT")."/magic/" . $period . "_index.log";
+$date_now = date("Y-m-d H:i:s");
+
 if($_SERVER['REQUEST_URI'] !== "/cart/incart/?display=ajax"){
     $str = date("Y-m-d H:i:s") . " || " . $_SERVER['REMOTE_ADDR'] . " || " . $_SERVER['HTTP_REFERER'] . " || " . $_SERVER['REQUEST_URI'] . " || " . $_SERVER["HTTP_USER_AGENT"] . "\r\n";
 }
-//if($_SERVER['REQUEST_URI'] == "/cart/incart/?display=ajax") unset $str;
-//$str = "..";
 // Пишем содержимое в файл,
 // используя флаг FILE_APPEND для дописывания содержимого в конец файла
 // и флаг LOCK_EX для предотвращения записи данного файла кем-нибудь другим в данное время
-file_put_contents($file, $str, FILE_APPEND | LOCK_EX);
+file_put_contents($index_log, $str, FILE_APPEND | LOCK_EX);
+?>
